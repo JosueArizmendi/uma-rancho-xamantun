cb8777c8976a33faa7dd546b29be8f62cb72edd4
JosueArizmendi <143550570+JosueArizmendi@users.noreply.github.com>
2025-10-24 08:44:52 -0600

    Update conexion_bd.php

--- model/conexion_bd.php
+++ model/conexion_bd.php
@@ -1,8 +1,8 @@
 <?php
-$servername="localhost";
-$dbname="uma_xamantun";
-$username="root";
-$password="";
+$servername = "bhf6s5kuuvyq21brrqgx-mysql.services.clever-cloud.com";
+$dbname = "bhf6s5kuuvyq21brrqgx";
+$username = "uohiphvlz2w7e1cj";
+$password = "cwjVQ1tJNeU8EdzN8ZcMI"; // 🔥 PON TU PASSWORD REAL
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
